bcb2d25096233332b65f61561dd33a1a6899254b
Rex.Chien <lovestar5317@gmail.com>
2019-02-16 09:06:11 +0800

    Method Signature. Assert Fail.

---
+++ LeetCodePractice/TwoSum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice
+{
+    public class TwoSum
+    {
+        public int[] Solution(int[] nums, int target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LeetCodePracticeTests/TwoSumTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodePractice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Tests
+{
+    [TestClass()]
+    public class TwoSumTests
+    {
+        [TestMethod()]
+        public void SolutionTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
