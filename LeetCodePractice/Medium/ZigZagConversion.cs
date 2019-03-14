857eb9bb2ed215853299543e0af1a6e8a2f92c22
Rex.Chien <lovestar5317@gmail.com>
2019-03-14 16:53:04 +0800

    Pass 2nd test clas

--- LeetCodePractice/Medium/ZigZagConversion.cs
+++ LeetCodePractice/Medium/ZigZagConversion.cs
@@ -6,7 +6,27 @@ namespace LeetCodePractice.Medium
     {
         public string Solution(string s, int numRows)
         {
-            return "PAHNAPLSIIGYIR";
+            string[] rows = new string[numRows];
+            int currentRow = 0;
+            int step = 1;
+            
+            foreach (var c in s)
+            {
+                rows[currentRow] += c;
+
+                if (currentRow == 0)
+                {
+                    step = 1;
+                }
+
+                if (currentRow == rows.Length - 1)
+                {
+                    step = -1;
+                }
+                
+                currentRow += step;
+            }
+            return string.Join("", rows);
         }
     }
 }
