9d991302460a728cec0405ac3c49b8b330675f48
Rex.Chien <lovestar5317@gmail.com>
2019-03-14 16:57:40 +0800

    Pass 4th test case

--- LeetCodePractice/Medium/ZigZagConversion.cs
+++ LeetCodePractice/Medium/ZigZagConversion.cs
@@ -6,6 +6,11 @@ namespace LeetCodePractice.Medium
     {
         public string Solution(string s, int numRows)
         {
+            if (numRows == 1)
+            {
+                return s;
+            }
+            
             string[] rows = new string[numRows];
             int currentRow = 0;
             int step = 1;
