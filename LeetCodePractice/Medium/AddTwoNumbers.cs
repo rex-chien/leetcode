6c94f9d47b06a83ff243c1fa9f6e5219a4246a93
Rex.Chien <lovestar5317@gmail.com>
2019-03-14 14:40:36 +0800

    Pass 3rd test case

--- LeetCodePractice/Medium/AddTwoNumbers.cs
+++ LeetCodePractice/Medium/AddTwoNumbers.cs
@@ -13,10 +13,10 @@ namespace LeetCodePractice.Medium
 
             while (t1 != null || t2 != null)
             {
-                int v1 = t1.val;
-                int v2 = t2.val;
-                t1 = t1.next;
-                t2 = t2.next;
+                int v1 = t1?.val ?? 0;
+                int v2 = t2?.val ?? 0;
+                t1 = t1?.next;
+                t2 = t2?.next;
 
                 int sum = v1 + v2 + exceedValue;
                 exceedValue = sum / 10;
