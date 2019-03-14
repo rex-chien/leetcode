5d7d80a59cc57a2a8cfd9aef6bf0f1a89561e7f0
Rex.Chien <lovestar5317@gmail.com>
2019-03-14 14:40:22 +0800

    Add 3rd test case

--- LeetCodePracticeTests/Medium/AddTwoNumberTests.cs
+++ LeetCodePracticeTests/Medium/AddTwoNumberTests.cs
@@ -20,6 +20,12 @@ namespace LeetCodePracticeTests.Medium
         {
             GivenTwoNumbersArrayShouldGet(new[] {1, 2, 3}, new[] {4, 5, 6}, new[] {5, 7, 9});
         }
+        
+        [TestMethod]
+        public void SolutionTest_l1_4_5_l2_7_5_2_Output_1_1_3()
+        {
+            GivenTwoNumbersArrayShouldGet(new[] {4, 5}, new[] {7, 5, 2}, new[] {1, 1, 3});
+        }
 
         private void GivenTwoNumbersArrayShouldGet(int[] numbers1, int[] numbers2, int[] expectedNumbers)
         {
