10f6715d35953ef1d8a0112496b4399a58d325d5
Rex.Chien <lovestar5317@gmail.com>
2019-03-14 13:41:33 +0800

    Add 2nd test case

--- LeetCodePracticeTests/Medium/AddTwoNumberTests.cs
+++ LeetCodePracticeTests/Medium/AddTwoNumberTests.cs
@@ -15,6 +15,12 @@ namespace LeetCodePracticeTests.Medium
             GivenTwoNumbersArrayShouldGet(new[] {2, 4, 3}, new[] {5, 6, 4}, new[] {7, 0, 8});
         }
 
+        [TestMethod]
+        public void SolutionTest_l1_1_2_3_l2_4_5_6_Output_5_7_9()
+        {
+            GivenTwoNumbersArrayShouldGet(new[] {1, 2, 3}, new[] {4, 5, 6}, new[] {5, 7, 9});
+        }
+
         private void GivenTwoNumbersArrayShouldGet(int[] numbers1, int[] numbers2, int[] expectedNumbers)
         {
             // arrange
