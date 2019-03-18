e994f3c5434681f490f2258032516b3c3943a6fc
Rex.Chien <lovestar5317@gmail.com>
2019-03-18 17:00:47 +0800

    Problem Initialized
    1. Add 1st test case.
    2. Add method signature.

---
+++ LeetCodePractice/Medium/QueueReconstrunctionByHeight.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace LeetCodePractice.Medium
+{
+    public class QueueReconstrunctionByHeight
+    {
+        public int[,] ReconstructQueue(int[,] people)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
