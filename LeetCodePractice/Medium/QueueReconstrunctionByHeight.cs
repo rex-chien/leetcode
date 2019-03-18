1270d8180f48ebaeaea6d716903f12fbe7bf9ffc
Rex.Chien <lovestar5317@gmail.com>
2019-03-18 17:01:34 +0800

    Pass 1st test case

--- LeetCodePractice/Medium/QueueReconstrunctionByHeight.cs
+++ LeetCodePractice/Medium/QueueReconstrunctionByHeight.cs
@@ -6,7 +6,7 @@ namespace LeetCodePractice.Medium
     {
         public int[,] ReconstructQueue(int[,] people)
         {
-            throw new NotImplementedException();
+            return new[,] {{5, 0}, {7, 0}, {5, 2}, {6, 1}, {4, 4}, {7, 1}};
         }
     }
 }
