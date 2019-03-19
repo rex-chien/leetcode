988f0c3557f6f3b8d6c1807b6a64dd9d625520f0
Rex.Chien <lovestar5317@gmail.com>
2019-03-19 09:42:08 +0800

    Fix typo

---
+++ LeetCodePracticeTests/Medium/QueueReconstructionByHeightTests.cs
@@ -0,0 +1,24 @@
+using LeetCodePractice.Medium;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodePracticeTests.Medium
+{
+    [TestClass]
+    public class QueueReconstructionByHeightTests
+    {
+        [TestMethod]
+        public void ReconstructQueueTest_()
+        {
+            // arrange
+            var sut = new QueueReconstructionByHeight();
+            var input = new[,] {{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}};
+            var expected = new[,] {{5, 0}, {7, 0}, {5, 2}, {6, 1}, {4, 4}, {7, 1}};
+
+            // act
+            var actual = sut.ReconstructQueue(input);
+            
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
