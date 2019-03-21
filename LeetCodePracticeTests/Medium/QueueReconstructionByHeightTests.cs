462f060bb1ab4d92406f4a12465981f686fe1972
Rex.Chien <lovestar5317@gmail.com>
2019-03-21 11:07:59 +0800

    Add 2nd test case

--- LeetCodePracticeTests/Medium/QueueReconstructionByHeightTests.cs
+++ LeetCodePracticeTests/Medium/QueueReconstructionByHeightTests.cs
@@ -14,6 +14,14 @@ namespace LeetCodePracticeTests.Medium
                 new[,] {{5, 0}, {7, 0}, {5, 2}, {6, 1}, {4, 4}, {7, 1}});
         }
 
+        [TestMethod]
+        public void ReconstructQueueTest_2()
+        {
+            GivenInputShouldExpectedOutput(
+                new[,] {{6, 2}, {8, 0}, {3, 3}, {4, 0}, {4, 5}, {6, 0}, {7, 1}},
+                new[,] {{4, 0}, {6, 0}, {8, 0}, {3, 3}, {6, 2}, {7, 1}, {4, 5}});
+        }
+
         private static void GivenInputShouldExpectedOutput(int[,] input, int[,] expected)
         {
             // arrange
