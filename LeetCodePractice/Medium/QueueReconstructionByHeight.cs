565573331a957ef408f12e04e8f352b09ef25d27
Rex.Chien <lovestar5317@gmail.com>
2019-04-03 14:32:13 +0800

    Pass 2nd test case

--- LeetCodePractice/Medium/QueueReconstructionByHeight.cs
+++ LeetCodePractice/Medium/QueueReconstructionByHeight.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LeetCodePractice.Medium
 {
@@ -6,7 +8,26 @@ namespace LeetCodePractice.Medium
     {
         public int[,] ReconstructQueue(int[,] people)
         {
-            return new[,] {{5, 0}, {7, 0}, {5, 2}, {6, 1}, {4, 4}, {7, 1}};
+            var output = new int[people.GetLength(0), 2];
+
+            var tuples = Enumerable.Range(0, people.GetLength(0))
+                .Select(i => new Tuple<int, int>(people[i, 0], people[i, 1]));
+            var sortedTuples = tuples.OrderByDescending(t => t.Item1).ThenBy(t => t.Item2);
+            var resultTuples = new List<Tuple<int, int>>();
+            
+            foreach (var tuple in sortedTuples)
+            {
+                resultTuples.Insert(tuple.Item2, tuple);
+            }
+            
+            for (var i = 0; i < resultTuples.Count; i++)
+            {
+                var tuple = resultTuples[i];
+                output[i, 0] = tuple.Item1;
+                output[i, 1] = tuple.Item2;
+            }
+
+            return output;
         }
     }
 }
