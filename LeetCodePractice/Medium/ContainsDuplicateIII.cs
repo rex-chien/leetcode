9e9a01aa51b154932676c91276e4a67f9e526bb5
Rex Chien <rex.chien.0509@gmail.com>
2022-03-09 10:12:53 +0800

    Update ContainsDuplicateIII.cs

--- LeetCodePractice/Medium/ContainsDuplicateIII.cs
+++ LeetCodePractice/Medium/ContainsDuplicateIII.cs
@@ -5,7 +5,10 @@ namespace LeetCodePractice.Medium
     public class ContainsDuplicateIII
     {
         /// <summary>
-        /// Given an array of integers, find out whether there are two distinct indices i and j in the array such that the absolute difference between nums[i] and nums[j] is at most t and the absolute difference between i and j is at most k.
+        /// Given an array of integers,
+        /// find out whether there are two distinct indices i and j in the array
+        /// such that the absolute difference between nums[i] and nums[j] is at most t
+        /// and the absolute difference between i and j is at most k.
         /// </summary>
         /// <param name="nums"></param>
         /// <param name="k"></param>
@@ -13,7 +16,23 @@ namespace LeetCodePractice.Medium
         /// <returns></returns>
         public bool ContainsNearbyAlmostDuplicate(int[] nums, int k, int t)
         {
-            return true;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                
+                
+                for (var j = i + 1; j < nums.Length && j <= i + k; j++)
+                {
+                    var num1 = nums[i];
+                    var num2 = nums[j];
+
+                    if (Math.Abs((long)num1 - num2) <= t)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
     }
 }
